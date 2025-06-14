e4d5eb649ee3c7987a4507229209579f7ff1ebc5
SkiuteDomantas <domantas.skiute@student.howest.be>
2025-06-14 14:17:12 +0200

    Added basic structure of the events page

---
+++ whispering_gardens/src/js/events.js
@@ -0,0 +1,13 @@
+ const menuButton = document.querySelector(".menu-btn");
+  const closeButton = document.querySelector("#closeNav");
+  const navOverlay = document.querySelector("#navOverlay")
+ 
+ menuButton.addEventListener("click", () => {
+    navOverlay.classList.add("active")
+    document.body.style.overflow = "hidden" 
+  })
+
+  closeButton.addEventListener("click", () => {
+    navOverlay.classList.remove("active")
+    document.body.style.overflow = "auto" 
+  })
